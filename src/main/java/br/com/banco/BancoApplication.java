7f5916c58648905f97eae8f9cc9797c8c006b083
Natan Lopes <natan1918@gmail.com>
2023-07-14 13:04:57 -0300

    teste

--- src/main/java/br/com/banco/BancoApplication.java
+++ src/main/java/br/com/banco/BancoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BancoApplication {
-
+///tete
     public static void main(String[] args) {
         SpringApplication.run(BancoApplication.class, args);
     }
