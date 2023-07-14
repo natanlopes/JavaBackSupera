775e3174b2ddf620bf83855e0c09d570e09d55d9
Natan Lopes <natan1918@gmail.com>
2023-07-14 15:13:02 -0300

    add commit transferencia

--- src/main/java/br/com/banco/ControllerAssistant.java
+++ src/main/java/br/com/banco/ControllerAssistant.java
@@ -1,5 +1,7 @@
 package br.com.banco;
-
+import java.time.LocalDateTime;
 public class ControllerAssistant {
-
+	public LocalDateTime dateTimeConverter(String string) {
+		return LocalDateTime.parse(string);
 }
+	}
---
+++ src/main/java/br/com/banco/TransferenciaControllerImpl.java
@@ -0,0 +1,25 @@
+package br.com.banco;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import br.com.banco.service.TransferenciaServiceImpl;
+
+public class TransferenciaControllerImpl {
+
+	public TransferenciaControllerImpl(TransferenciaServiceImpl transferenciaServiceImpl) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Object listar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public <Transferencia> List<Transferencia> filtered(LocalDateTime dateTimeConverter, LocalDateTime dateTimeConverter2,
+			String nomeOperador) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
