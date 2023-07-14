66815449ac052cf995c412b7520c9b9a0e3bbef2
Natan Lopes <natan1918@gmail.com>
2023-07-14 12:40:27 -0300

    add commit inicial

--- src/test/java/br/com/banco/BancoApplicationTests.java
+++ src/test/java/br/com/banco/BancoApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BancoApplicationTests {
-// criando conexao  com banco
+// criand
     @Test
     void contextLoads() {
     }
